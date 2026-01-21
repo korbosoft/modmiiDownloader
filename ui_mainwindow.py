433b16edf695a2098839b5a8b74cfe7798ae1fbe
Korbo <korbin.m.marshall@gmail.com>
2026-01-20 18:10:33 -0600

    init 3... god help us all

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'main.ui'
+## Form generated from reading UI file 'mainwindow.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.10.1
 ##
